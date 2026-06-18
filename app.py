d2cfb0175ca3d5f6366da3828cd776305442ea29
Tejas Manindra <tejasmani2002@gmail.com>
2026-06-18 13:23:26 +0530

    Added chunking and gitignore

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from backend.pdf_loader import extract_text_from_pdf
+from backend.chunker import create_chunks
 
 pdf_path = "data/uploads/cnn_rnn_sample.pdf"
 
 text = extract_text_from_pdf(pdf_path)
 
-print(text[:2000])
+chunks = create_chunks(text)
+
+print(f"Total Chunks: {len(chunks)}")
+
+print(chunks[0])
